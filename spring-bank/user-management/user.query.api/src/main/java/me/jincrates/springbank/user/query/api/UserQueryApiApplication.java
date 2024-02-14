a56aea49fd1f55e585332aa5c0d2bc06c17b10d7
jincrates <94jingyu@gmail.com>
2024-02-14 16:54:04 +0900

    init project

---
+++ spring-bank/user-management/user.query.api/src/main/java/me/jincrates/springbank/user/query/api/UserQueryApiApplication.java
@@ -0,0 +1,13 @@
+package me.jincrates.springbank.user.query.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCommandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCommandApiApplication.class, args);
+	}
+
+}
---
+++ spring-bank/user-management/user.query.api/src/test/java/me/jincrates/springbank/user/query/api/UserQueryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jincrates.springbank.user.query.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
